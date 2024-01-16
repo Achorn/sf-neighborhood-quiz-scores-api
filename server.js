ee1b4fcfe5c99a74a3a0de615fd675b2ca6e19b5
achorn <joshuaachorn@gmail.com>
2024-01-16 13:59:08 -0800

    set up qsudocode for future get and post

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ app.get("/api", (req, res) => {
   res.json({ greeting: "Hello, world!" });
 });
 
+//get scores limit
+app.get("/api/scores", (req, res) => {
+  //get scores from mongo db
+  // send json of top 10 scores
+  res.json({ scores: "scores go here" });
+});
+
+app.post("/api/score", (req, res) => {
+  //create new score from model
+  //  save score
+  // send error if fail
+  // send response if sucess
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });

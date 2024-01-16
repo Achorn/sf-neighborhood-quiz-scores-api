3e84ae9f1545d0c137e3c4443f5a3f78c8957364
achorn <joshuaachorn@gmail.com>
2024-01-16 14:13:48 -0800

    added restrictions to schema

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const mongoose = require("mongoose");
 
 const scoreSchema = mongoose.Schema({
   username: { type: String, required: true, maxLength: 10 },
-  score: { type: Number, required: true },
-  time: { type: Number, required: true },
+  score: { type: Number, required: true, min: 0, max: 1 },
+  time: { type: Number, required: true, min: 0 },
   date: { type: Date, default: Date.now },
 });
 
